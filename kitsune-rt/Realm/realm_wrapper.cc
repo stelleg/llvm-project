44cbace0d2e375eded959c03f0b7bbab8132f69b
Alexis Perry-Holby <aperry@lanl.gov>
2020-04-14 15:40:06 -0600

    Added Realm link libs and code cleanup
    - Added "-lhwloc -lnuma -lpthread" to REALM_LINK_LIBS to mirror what the QThreads backend does
    - Code cleanup of other files, removing comments and large unused blocks of code

--- kitsune-rt/Realm/realm_wrapper.cc
+++ kitsune-rt/Realm/realm_wrapper.cc
@@ -67,7 +67,6 @@ extern "C" {
        needs pointer to user data and arguments (NULL for void?)
        needs size_t for len (0 for void?)
     */
-    
     context *ctx = getRealmCTX();
 
     Realm::Processor::TaskFuncID taskID = ctx->cur_task;
@@ -75,11 +74,6 @@ extern "C" {
     //get a processor to run on
     Realm::Processor p = ctx->procgroup; //spawn on the group to enable Realm's magic load-balancing
     //Realm::Processor p = (ctx->procs)[i]; //do round-robin spawning on the vector of procs (needs i calculated)
-
-    //get a memory associated with that processor to copy to
-    //Realm::Machine::MemoryQuery memquery(Realm::Machine::get_machine());
-    //Realm::Memory m = memquery.local_address_space().best_affinity_to(p).random();
-    //assert ( m != Realm::Memory::NO_MEMORY); //assert that the memory exists
 
     // Create a CodeDescriptor from the TaskFuncPtr   
     Realm::CodeDescriptor cd = Realm::CodeDescriptor(func);
@@ -113,146 +107,3 @@ extern "C" {
     return 0;
   }
 }
-
-/// BEGIN EXTRANEOUS STUFF THAT I'M NOT READY TO THROW AWAY YET
-#if 0
-//realmCreateRegion
-void* realmCreateRegion_int(int* data) {
-
-  context * ctx = getRealmCTX();
-
-  const Realm::ProfilingRequestSet prs;  //We don't care what it is for now, the default is fine
-  std::cout << "Created default prs" << std::endl;
-
-  //get a processor to run on
-  Realm::Machine::ProcessorQuery procquery(Realm::Machine::get_machine());
-  Realm::Processor p = procquery.local_address_space().random();
-  assert ( p != Realm::Processor::NO_PROC); //assert that the processor exists
-  std::cout << "Got a processor" << std::endl;
-
-  //get a memory associated with that processor to copy to
-  Realm::Machine::MemoryQuery memquery(Realm::Machine::get_machine());
-  Realm::Memory m = memquery.local_address_space().best_affinity_to(p).random();
-  assert ( m != Realm::Memory::NO_MEMORY); //assert that the memory exists
-  std::cout << "Got a memory" << std::endl;
-
-  //create a physical region for the copy
-  //Realm::RegionInstance * R = new Realm::RegionInstance();
-  Realm::RegionInstance * R = m.get_instance(id(Realm::Event::NO_EVENT))
-    std::cout << "Created default RegionInstance" << std::endl;
-
-  //create a point object out of the data being passed
-  const size_t length = sizeof(data)/sizeof(data[0]);
-  Realm::Point<length,int> pt = Realm::Point<length,int>(data);
-  std::cout << "Created Point" << std::endl;
-
-  //create an indexspace out of the point
-  std::vector<Realm::Point<length, int> > myPointVec;
-  myPointVec.push_back(pt);
-  const Realm::IndexSpace<length,int> is = Realm::IndexSpace<length,int>(myPointVec);
-  std::cout << "Created IndexSpace" << std::endl;
-
-  //create a vector of field sizes
-  std::vector<size_t> field_sizes = {sizeof(data[0])}; //data is an array of ints, so there is only one field
-  std::cout << "Created field_sizes" << std::endl;
-  //constexpr auto user_data_type = std::type_index(DTYPE);
-  //constexpr auto user_data_type = (constexpr)DTYPE.name();
-  //Realm::InstanceLayout<user_data_len,typeid(user_data[0]).name()> il;
-  //Realm::InstanceLayout<1,user_data_type> il = Realm::InstanceLayoutOpaque(user_data_len,alignof(user_data)); //alignment is what?
-  //Realm::InstanceLayout<1,typeid(user_element).name()> il = Realm::InstanceLayoutOpaque(user_data_len,alignof(user_data)); //alignment is what?
-  //const Realm::InstanceLayoutGeneric * il = ((Realm::RegionInstance *)data_region)->get_layout(); //copy the layout of the source region
-
-  //Realm::Event regEvt = Realm::RegionInstance::create_instance(R,m,(Realm::InstanceLayoutGeneric *)il,prs, Realm::Event::NO_EVENT);
-  Realm::Event regEvt = Realm::RegionInstance::create_instance(*R, m, is, field_sizes, 0, prs, Realm::Event::NO_EVENT); //the 0 denotes use SOA layout
-  std::cout << "Executed create_instance" << std::endl;
-  ctx->mem_events.insert(regEvt);
-  std::cout << "Inserted creation instance into ctx->mem_events" << std::endl;
-
-  return (void*) R;
-}
-
-void realmDestroyRegion(void *region) {
-  //region->destroy(*event);
-  ((Realm::RegionInstance *)region)->destroy(Realm::Event::NO_EVENT); //destroys immediately
-  return;
-}
-
-//only use this internally
-Realm::Event mem_sync();
-Realm::Event mem_sync() {
-  context * ctx = getRealmCTX();
-  Realm::Event e;
-  e = e.merge_events(ctx->mem_events);
-  std::cout << "merged memory events" << std::endl;
-
-  ctx->mem_events.clear();
-  std::cout << "Cleared the context's set of memory events" << std::endl;
-  ctx->mem_events.insert(e);
-  std::cout << "Added mem_sync event to context's set of memory events" << std::endl;
-
-  return e;
-}
-
-//only use internally
-//Note: borrowed this routine from https://github.com/StanfordLegion/legion/blob/stable/examples/realm_stencil/realm_stencil.cc
-Realm::Event realmCopy_int(Realm::RegionInstance src_inst, 
-			   Realm::RegionInstance dst_inst, 
-			   Realm::FieldID fid, //int
-			   Realm::Event wait_for) {
-  Realm::CopySrcDstField src_field;
-  src_field.inst = src_inst;
-  src_field.field_id = fid;
-  src_field.size = sizeof(int);
-
-  std::vector<Realm::CopySrcDstField> src_fields;
-  src_fields.push_back(src_field);
-
-  Realm::CopySrcDstField dst_field;
-  dst_field.inst = dst_inst;
-  dst_field.field_id = fid;
-  dst_field.size = sizeof(int);
-
-  std::vector<Realm::CopySrcDstField> dst_fields;
-  dst_fields.push_back(dst_field);
-
-  return dst_inst.get_indexspace<2, long long int>().copy(src_fields, dst_fields,
-							  Realm::ProfilingRequestSet(),
-							  wait_for);
-}
-
-//only use internally
-//Note: borrowed this routine from https://github.com/StanfordLegion/legion/blob/stable/examples/realm_stencil/realm_stencil.cc
-Realm::Event realmCopy_double(Realm::RegionInstance src_inst, 
-			      Realm::RegionInstance dst_inst, 
-			      Realm::FieldID fid, //int
-			      Realm::Event wait_for) {
-  Realm::CopySrcDstField src_field;
-  src_field.inst = src_inst;
-  src_field.field_id = fid;
-  src_field.size = sizeof(double);
-
-  std::vector<Realm::CopySrcDstField> src_fields;
-  src_fields.push_back(src_field);
-
-  Realm::CopySrcDstField dst_field;
-  dst_field.inst = dst_inst;
-  dst_field.field_id = fid;
-  dst_field.size = sizeof(double);
-
-  std::vector<Realm::CopySrcDstField> dst_fields;
-  dst_fields.push_back(dst_field);
-
-  return dst_inst.get_indexspace<2, long long int>().copy(src_fields, dst_fields,
-							  Realm::ProfilingRequestSet(),
-							  wait_for);
-}
-
-//helper - makes a Realm::Processor::TaskFuncPtr out of available things
-void canonicalTask(const void *args, size_t arglen, void *user_data, size_t user_data_len, Realm::Processor p);
-void canonicalTask(const void *args, size_t arglen, void *user_data, size_t user_data_len, Realm::Processor p){}
-Realm::Processor::TaskFuncPtr makeCanonicalTask(const void *args, size_t arglen, void *user_data, size_t user_data_len, Realm::Processor p);
-makeCanonicalTask(const void *args, size_t arglen, void *user_data, size_t user_data_len, Realm::Processor p){
-  Realm::Processor::TaskFuncPtr task = 
-    return task;
-}
-#endif
--- llvm/lib/Transforms/Tapir/RealmABI.cpp
+++ llvm/lib/Transforms/Tapir/RealmABI.cpp
@@ -29,7 +29,7 @@ FunctionCallee RealmABI::get_realmGetNumProcs() {
   if(RealmGetNumProcs)
     return RealmGetNumProcs;
 
-  LLVMContext &C = M.getContext(); //TODO: where does M come from?
+  LLVMContext &C = M.getContext(); 
   const DataLayout &DL = M.getDataLayout();
   AttributeList AL;
   // TODO: Set appropriate function attributes.
@@ -42,7 +42,7 @@ FunctionCallee RealmABI::get_realmSpawn() {
   if(RealmSpawn)
     return RealmSpawn;
 
-  LLVMContext &C = M.getContext(); //TODO: where does M come from?
+  LLVMContext &C = M.getContext(); 
   const DataLayout &DL = M.getDataLayout();
   AttributeList AL;
   // TODO: Set appropriate function attributes.
@@ -62,7 +62,7 @@ FunctionCallee RealmABI::get_realmSync() {
   if(RealmSync)
     return RealmSync;
 
-  LLVMContext &C = M.getContext(); //TODO: where does M come from?
+  LLVMContext &C = M.getContext(); 
   AttributeList AL;
   // TODO: Set appropriate function attributes.
   FunctionType *FTy = FunctionType::get(Type::getInt32Ty(C), {}, false);
@@ -74,7 +74,7 @@ FunctionCallee RealmABI::get_realmInitRuntime() {
   if(RealmInitRuntime)
     return RealmInitRuntime;
 
-  LLVMContext &C = M.getContext(); //TODO: where does M come from?
+  LLVMContext &C = M.getContext(); 
   AttributeList AL;
   // TODO: Set appropriate function attributes.
   FunctionType *FTy = FunctionType::get(
@@ -209,141 +209,8 @@ void RealmABI::processSubTaskCall(TaskOutlineInfo &TOI, DominatorTree &DT) {
   // function to manage the allocation of the argument structure.
 }
 
-#if 0
-// Changes the Outlined function from Tapir into the right form
-// for use with the Realm runtime
-Function* RealmABI::formatFunctionToRealmF(Function* extracted, Instruction* ical){
-  std::vector<Value*> LoadedCapturedArgs;
-  CallInst *cal = dyn_cast<CallInst>(ical);
-
-  for(auto& a:cal->arg_operands()) {
-    LoadedCapturedArgs.push_back(a);
-  }
-
-  Module *M = extracted->getParent(); 
-  auto& C = M->getContext(); 
-  DataLayout DL(M);
-  IRBuilder<> CallerIRBuilder(cal);
-
-  //get the argument types
-  auto FnParams = extracted->getFunctionType()->params();
-  StructType *ArgsTy = StructType::create(FnParams, "anon");
-  auto *ArgsPtrTy = PointerType::getUnqual(ArgsTy);
-
-  //Create the canonical TaskFuncPtr
-  ArrayRef<Type*> typeArray = {Type::getInt8PtrTy(C), Type::getInt64Ty(C), Type::getInt8PtrTy(C), Type::getInt64Ty(C), Type::getInt64Ty(C)}; //trying int64 as stand-in for Realm::Processor because a ::realm_id_t is ultimately an unsigned long long
-
-  FunctionType *OutlinedFnTy = FunctionType::get(
-      Type::getInt8Ty(C), 
-      typeArray,
-      false);
-
-  Function *OutlinedFn = Function::Create(OutlinedFnTy, GlobalValue::InternalLinkage, ".realm_outlined.", M);
-  OutlinedFn->addFnAttr(Attribute::AlwaysInline);
-  OutlinedFn->addFnAttr(Attribute::NoUnwind);
-  OutlinedFn->addFnAttr(Attribute::UWTable);
-
-  //StringRef ArgNames[] = {".args"};
-  std::vector<Value*> out_args;
-  for (auto &Arg : OutlinedFn->args()) {
-    //Arg.setName(ArgNames[out_args.size()]);
-    Arg.setName("");
-    out_args.push_back(&Arg);
-  }
-
-  // Entry Code
-  auto *EntryBB = BasicBlock::Create(C, "entry", OutlinedFn, nullptr);
-  IRBuilder<> EntryBuilder(EntryBB);
-  auto argStructPtr = EntryBuilder.CreateBitCast(out_args[0], ArgsPtrTy); 
-  ValueToValueMapTy valmap;
-
-  unsigned int argc = 0;
-  for (auto& arg : extracted->args()) {
-    auto *DataAddrEP = EntryBuilder.CreateStructGEP(ArgsTy, argStructPtr, argc); 
-    auto *DataAddr = EntryBuilder.CreateAlignedLoad(
-        DataAddrEP,
-        DL.getTypeAllocSize(DataAddrEP->getType()->getPointerElementType()));
-    valmap.insert(std::pair<Value*,Value*>(&arg,DataAddr));
-    argc++;
-  }
-
-  // Replace return values with return zero 
-  SmallVector< ReturnInst *,5> retinsts;
-  CloneFunctionInto(OutlinedFn, extracted, valmap, true, retinsts);
-  EntryBuilder.CreateBr(OutlinedFn->getBasicBlockList().getNextNode(*EntryBB));
-
-  for (auto& ret : retinsts) {
-    auto retzero = ReturnInst::Create(C, ConstantInt::get(Type::getInt8Ty(C), 0));
-    ReplaceInstWithInst(ret, retzero);
-  }
-
-  // Caller code
-  auto callerArgStruct = CallerIRBuilder.CreateAlloca(ArgsTy); 
-
-  unsigned int cArgc = 0;
-  for (auto& arg : LoadedCapturedArgs) {
-    auto *DataAddrEP2 = CallerIRBuilder.CreateStructGEP(ArgsTy, callerArgStruct, cArgc); 
-    CallerIRBuilder.CreateAlignedStore(
-        LoadedCapturedArgs[cArgc], DataAddrEP2,
-        DL.getTypeAllocSize(arg->getType()));
-    cArgc++;
-  }
-
-  assert(argc == cArgc && "Wrong number of arguments passed to outlined function"); 
-
-  auto outlinedFnPtr = CallerIRBuilder.CreatePointerBitCastOrAddrSpaceCast(
-  			       	   OutlinedFn, TypeBuilder<TaskFuncPtr, false>::get(M->getContext())); 
-  auto argSize = ConstantInt::get(Type::getInt64Ty(C), ArgsTy->getNumElements()); 
-  auto argDataSize = ConstantInt::get(Type::getInt64Ty(C), DL.getTypeAllocSize(ArgsTy)); 
-  auto argsStructVoidPtr = CallerIRBuilder.CreateBitCast(callerArgStruct, Type::getInt8PtrTy(C)); 
-
-  ArrayRef<Value *> callerArgs = { outlinedFnPtr, argsStructVoidPtr, argSize, argsStructVoidPtr, argDataSize}; 
-
-  CallerIRBuilder.CreateCall(REALM_FUNC(realmSpawn, *M), callerArgs); 
-
-  cal->eraseFromParent();
-  extracted->eraseFromParent();
-
-  LLVM_DEBUG(OutlinedFn->dump()); 
-
-  return OutlinedFn; 
-}
-
-Function *RealmABI::createDetach(DetachInst &detach,
-				 ValueToValueMapTy &DetachCtxToStackFrame,
-				 DominatorTree &DT, AssumptionCache &AC) {
-  BasicBlock *detB = detach.getParent();
-  Function &F = *(detB->getParent());
-  BasicBlock *Spawned  = detach.getDetached();
-  BasicBlock *Continue = detach.getContinue();
-
-  Instruction *cal = nullptr;
-  Function *extracted = extractDetachBodyToFunction(detach, DT, AC, &cal);
-  extracted = formatFunctionToRealmF(extracted, cal); 
-
-  // Replace the detach with a branch to the continuation.
-  BranchInst *ContinueBr = BranchInst::Create(Continue);
-  ReplaceInstWithInst(&detach, ContinueBr);
-
-  // Rewrite phis in the detached block.
-  {
-    BasicBlock::iterator BI = Spawned->begin();
-    while (PHINode *P = dyn_cast<PHINode>(BI)) {
-      P->removeIncomingValue(detB);
-      ++BI;
-    }
-  }
-
-  LLVM_DEBUG(F.dump()); 
-
-  return extracted;
-}
-#endif
-
 void RealmABI::preProcessFunction(Function &F, TaskInfo &TI,
 				  bool OutliningTapirLoops) {
-  // TODO: Does this effectively put a barrier at the end of every Function?
-  // Or is it just the root task?  Because that might be ok...
 
   if (OutliningTapirLoops)
     // Don't do any preprocessing when outlining Tapir loops.
@@ -377,9 +244,6 @@ void RealmABI::postProcessFunction(Function &F, bool OutliningTapirLoops) {
   //TODO: handle the case where main actually has an argc and argv
   Value* one = ConstantInt::get(Type::getInt32Ty(C), 1);
   Value* null = Constant::getNullValue(PointerType::getUnqual(Type::getInt8PtrTy(C)));
-
-  one->dump();
-  null->dump();
 
   ArrayRef<Value*> initArgs = {one, null};
 

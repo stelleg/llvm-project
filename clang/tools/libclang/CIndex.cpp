903b4aedf9984d298a9ac7d96a8dffaf01dc6ba3
Patrick McCormick <pat@lanl.gov>
2020-06-15 13:02:23 -0600

    Working on kitsune'izing components to defferentiate them from tapir a bit (most of these simply require a tapir->kitsune renaming).  Some of this is separate modes of operation and the fact that our code generation and feature set are slightly different from tapir.
    Also addressed a few build issues for testing and such.

--- clang/tools/libclang/CIndex.cpp
+++ clang/tools/libclang/CIndex.cpp
@@ -5419,6 +5419,8 @@ CXString clang_getCursorKindSpelling(enum CXCursorKind Kind) {
     return cxstring::createRef("DoStmt");
   case CXCursor_ForStmt:
     return cxstring::createRef("ForStmt");
+  case CXCursor_ForallStmt:
+    return cxstring::createRef("ForallStmt");      
   case CXCursor_GotoStmt:
     return cxstring::createRef("GotoStmt");
   case CXCursor_IndirectGotoStmt:
@@ -5453,6 +5455,8 @@ CXString clang_getCursorKindSpelling(enum CXCursorKind Kind) {
     return cxstring::createRef("CXXTryStmt");
   case CXCursor_CXXForRangeStmt:
     return cxstring::createRef("CXXForRangeStmt");
+  case CXCursor_CXXForallRangeStmt:
+    return cxstring::createRef("CXXForallRangeStmt");      
   case CXCursor_SEHTryStmt:
     return cxstring::createRef("SEHTryStmt");
   case CXCursor_SEHExceptStmt:

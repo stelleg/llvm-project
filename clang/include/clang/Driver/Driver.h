023abaef3335d3f1d3d23c5c92aabc14bbf4fcf3
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-07-26 09:40:12 -0600

    Overhaul of some build mechanisms:
       1. Big change: build kitsune as a runtime component vs. subset of
          clang. This is required to build runtime components using the
          built clang (needed for bitcode runtime interfaces).
       2. #1 required some logic changes in runtime selection and automatic
          runtime flag generation/configuration (still working on this and
          will likely use --config as a likely solution path).  More details
          to come.
       3. Made error handling a bit more robust for the OpenCilkABI bitcode
          cases (especially when built without LLVM assertions).

--- clang/include/clang/Driver/Driver.h
+++ clang/include/clang/Driver/Driver.h
@@ -69,7 +69,7 @@ class Driver {
     CPPMode,
     CLMode,
     FlangMode,
-    KitsuneMode, 
+    KitsuneMode,
     KokkosMode,
     FleCSIMode
   } Mode;
@@ -145,6 +145,9 @@ public:
   /// User directory for config files.
   std::string UserConfigDir;
 
+  /// Kitsune directory for config files.
+  std::string KitsuneConfigDir;
+
   /// A prefix directory used to emulate a limited subset of GCC's '-Bprefix'
   /// functionality.
   /// FIXME: This type of customization should be removed in favor of the
@@ -183,9 +186,9 @@ public:
   using InputList = SmallVector<InputTy, 16>;
 
   /// Whether the driver should follow g++ like behavior.
-  /// kitsune: Modified to reflect that Kokkos and FleCSI 
-  /// modes also imply GXXMode. 
-  bool CCCIsCXX() const { return Mode == GXXMode    || 
+  /// kitsune: Modified to reflect that Kokkos and FleCSI
+  /// modes also imply GXXMode.
+  bool CCCIsCXX() const { return Mode == GXXMode    ||
                                  Mode == KokkosMode ||
                                  Mode == FleCSIMode; }
 
@@ -208,7 +211,7 @@ public:
   /// Whether the driver should follow custom Kokkos behaviors.
   bool IsKokkosMode() const { return Mode == KokkosMode; }
 
-  /// kitsune: Whether the driver should follow custom FleCSI behaviors. 
+  /// kitsune: Whether the driver should follow custom FleCSI behaviors.
   bool isFleCSIMode() const { return Mode == FleCSIMode; }
 
   /// Only print tool bindings, don't build any jobs.

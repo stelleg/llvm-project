e255f903b876bd9803fa78feb785acda8e36236d
Patrick McCormick <651611+pmccormick@users.noreply.github.com>
2021-08-16 12:30:52 -0600

    More work to sort through adding the Realm ABI target into the build system w/ realm as a FetchContent component.  This means some dependency and target work within cmake, some ordering details about where the FetchContent occurs, and also some fixes to the code base that were lost for realm support (e.g., missing switch entries).
    There is some point where the overall cmake config becomes more difficult
    to reason about than the llvm+clang+... source code.  ;-)
    
    A few additional fixes for the config file (.cfg) settings and supporting
    cmake pieces.  The default config files are autogenerated to handle both
    the in-tree and install use cases.  It is probably worth tailoring for
    your own use cases at some point but adding them to your user directory.
    
    TODO: there is currently a "collision" between the use of a cmake option
    and the ABI libraries to build within kitsune.  At present you will have
    both enable and add the abi library to the configuration (see the cache
    file under kitsune/cmake/caches/kitsune-dev.cmake).

--- clang/lib/Driver/ToolChains/Darwin.cpp
+++ clang/lib/Driver/ToolChains/Darwin.cpp
@@ -3247,6 +3247,7 @@ void DarwinClang::AddLinkTapirRuntime(const ArgList &Args,
                            !StaticOpenCilk);
     break;
   }
+  /*
   case TapirTargetID::Cilk:
     CmdArgs.push_back("-lcilkrts");
     break;
@@ -3257,9 +3258,10 @@ void DarwinClang::AddLinkTapirRuntime(const ArgList &Args,
     CmdArgs.push_back("-lqthread");
     break;
   case TapirTargetID::Realm:
+    CmdArgs.push_back("-lrealm-abi");
     CmdArgs.push_back("-lrealm");
-    CmdArgs.push_back("-lkitsunerealm");
     break;
+  */
   default:
     break;
   }

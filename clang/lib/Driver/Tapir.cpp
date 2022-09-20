tarunprabhu/kitsune

Added support for opencilk targets that follow the general kitsune pattern.
  -- new cmake support for opencilk
  -- fixed a missing codegen path and general support for --fkokkos-no-init
     mode
  -- fixed a -fopencilk vs. -ftapir=opencilk issue related to extra libraries
     being added that are not needed for -ftapir=opencilk support

clang/lib/Driver/Tapir.cpp
@@ -38,14 +38,16 @@ TapirTargetID clang::parseTapirTarget(const ArgList &Args) {
     TapirTarget = llvm::StringSwitch<TapirTargetID>(A->getValue())
       .Case("none", TapirTargetID::None)
       .Case("serial", TapirTargetID::Serial)
-      .Case("cheetah", TapirTargetID::Cheetah)
       .Case("cilk", TapirTargetID::Cilk)
-      .Case("cilkr", TapirTargetID::CilkR)
+      .Case("cilkr", TapirTargetID::CilkR)      
+      .Case("cheetah", TapirTargetID::Cheetah)
       .Case("cuda", TapirTargetID::Cuda)
-      .Case("opencilk", TapirTargetID::OpenCilk)
+      .Case("kitcuda", TapirTargetID::KitCuda) 
+      .Case("opencilk", TapirTargetID::OpenCilk)      
       .Case("openmp", TapirTargetID::OpenMP)
       .Case("qthreads", TapirTargetID::Qthreads)
-      .Case("realm", TapirTargetID::Realm)      
+      .Case("realm", TapirTargetID::Realm)
+      .Case("opencl", TapirTargetID::OpenCL) 
       .Default(TapirTargetID::Last_TapirTargetID);
 
   return TapirTarget;

stelleg/llvm-project

clang/lib/Driver/Tapir.cpp
@@ -38,13 +38,14 @@ TapirTargetID clang::parseTapirTarget(const ArgList &Args) {
     TapirTarget = llvm::StringSwitch<TapirTargetID>(A->getValue())
       .Case("none", TapirTargetID::None)
       .Case("serial", TapirTargetID::Serial)
-      .Case("cheetah", TapirTargetID::Cheetah)
-      .Case("cilkplus", TapirTargetID::Cilk)
+      .Case("cilk", TapirTargetID::Cilk)
       .Case("cuda", TapirTargetID::Cuda)
-      .Case("opencilk", TapirTargetID::OpenCilk)
+      .Case("kitcuda", TapirTargetID::KitCuda) 
+      .Case("opencilk", TapirTargetID::OpenCilk)      
       .Case("openmp", TapirTargetID::OpenMP)
       .Case("qthreads", TapirTargetID::Qthreads)
-      .Case("realm", TapirTargetID::Realm)      
+      .Case("realm", TapirTargetID::Realm)
+      .Case("opencl", TapirTargetID::OpenCL) 
       .Default(TapirTargetID::Last_TapirTargetID);
 
   return TapirTarget;